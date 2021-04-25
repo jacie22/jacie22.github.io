ef3d6d8161e9ec4b55813f830f94d1dc4a930906
jacie22 <jacie22@gmail.com>
2021-04-24 21:46:33 -0700

    lesson 1push

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let lastModified = document.lastModified;
+document.querySelector("#modDate").textContent = lastModified;
+let year = new Date().getFullYear();
+document.querySelector("#currentYear").textContent = year;
+
+
+
+
+
+
+
+
